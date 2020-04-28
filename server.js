0851177d94230916de53a7d5c02925487d616115
Danny Park <dannyjp98@gmail.com>
2020-04-28 16:32:39 -0400

    yeet

--- server.js
+++ server.js
@@ -92,6 +92,7 @@ app.post('/users/login', async function(req, res){
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, (req, res) => {
-    console.log('Server Started at PORT ${PORT}');
+    console.log('Server Started at PORT ' + PORT);
   });
